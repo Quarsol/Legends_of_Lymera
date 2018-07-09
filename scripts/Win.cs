d6990a9bbc66661d855fb4368e7700a1123cb2cd
Andrew Smith <Quarsol@yahoo.com>
2018-07-09 09:35:18 -0600

    Update Win.cs

---
+++ scripts/Win.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WIN : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space")) {
+			Destroy(GameObject.Find("Enemy"));
+		}
+	}
+}
